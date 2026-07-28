abce3fbad3bdd66be24056c5ffb6a7769c865276
angelgabrielmattos <angelgabriel-mattos@outlook.com>
2026-07-28 18:23:45 -0300

    add: letter density

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react"
 import { Header } from "./components/Header"
 
-function App() {
-  const [text, setText] = useState("Esto es un texto de prueba, puedes borrarlo, modificarlo o comprobar que la app esta funcionando correctamente.")
+const App = () => {
+  const [text, setText] = useState("")
+
   const [excludeSpaces, setExcludeSpaces] = useState(false)
   const [limitCharacter, setLimitCharacter] = useState(false)
-  const [limitValue, setLimitValue] = useState(50)
+  const [limitValue, setLimitValue] = useState(300)
+  const [showAll, setShowAll] = useState(false)
 
   const characteres = excludeSpaces ? text.replace(/\s/g, "").length : text.length
 
@@ -13,10 +15,13 @@ function App() {
 
   const sentences = text.trim() === "" ? 0 : text.split(/[.!?]/).filter(sentence => sentence.trim() !== "").length
 
+  const readingTime = Math.ceil (words / 180)
+
   const handleChangeTextarea = (e) => {
     const value = e.target.value
 
     if (limitCharacter) {
+      
       if (value.length <= limitValue) {
         setText(value)
       }
@@ -31,15 +36,41 @@ function App() {
     setText(newText)
   }
 
+  const cleanText = text.toLowerCase().replace(/[^a-záéíóúñ]/g, "")
+  const total = cleanText.length
+
+  const dictionaryLetters = {}
+
+  cleanText.split("").forEach(letter => {
+    dictionaryLetters[letter] = (dictionaryLetters[letter] || 0) + 1
+  })
+
+  const letters = Object.entries(dictionaryLetters).map(dataLetter => {
+    const letter = dataLetter[0]
+    const amountLetter = dataLetter[1]
+
+    const infoToRenderLetter = {
+      letterName: letter,
+      amount: amountLetter,
+      percentage: (amountLetter / total ) * 100
+    }
+
+    return infoToRenderLetter
+  })
+
+  const sortLetters = letters.sort((a,b) => b.amount - a.amount)
+
+  const visibleLetters = showAll ? sortLetters : sortLetters.slice(0, 5)
+  
   return (
     <main>
       <Header />
       <h2>Analyze your text <br /> 
-        in real-time</h2>
-        {/* <p>{text}</p> */}
+        in real-time.</h2>
+
         <textarea 
         placeholder="Escribe tu texto..."
-        onChange={(e) => setText(e.target.value)}
+        onChange={handleChangeTextarea}
         value={text}
         ></textarea>
         <div>
@@ -47,8 +78,7 @@ function App() {
           <input 
             type="checkbox" 
             checked={excludeSpaces}
-            onChange={() => setExcludeSpaces
-            (!excludeSpaces)}
+            onChange={() => setExcludeSpaces(!excludeSpaces)}
           />
           Exclude Spaces
           </label>
@@ -68,9 +98,24 @@ function App() {
             />
           }
         </div>
-        <p>Total Characters: {characteres}</p>
-        <p>Word Count: {words}</p>
-        <p>Sentence Count: {sentences}</p>
+        <p>Total Characters {characteres}</p>
+        <p>Word Count {words}</p>
+        <p>Sentence Count {sentences}</p>
+        <p>Approx. reading time: &lt;{readingTime}minute</p>
+        <section>
+          <h2>Letter Density</h2>
+          <button onClick={() => setShowAll(!showAll)}>{showAll ? "See less ▲" : "See more ▼"}</button>
+          <article>
+            {
+            sortLetters.slice(0, 5).map(letter => (
+              <div key={letter.letterName}>
+                <span>{letter.letterName.toUpperCase()}</span>
+                <meter min="0" max="100" value={letter.percentage}></meter>
+                <span>{letter.amount} ({letter.percentage.toFixed(1)}%)</span>
+              </div>))
+          }  
+          </article>
+        </section>
     </main>
   )
 }
